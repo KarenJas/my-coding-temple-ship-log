0a4f22d44646001f054624df7c3bda9082e0174c
KarenJas <kjgonzalez445@gmail.com>
2024-03-04 12:26:56 -0600

    updating

--- exercises/M2_L4.py
+++ exercises/M2_L4.py
@@ -59,15 +59,48 @@ while battery_life > 0 :
         else:
             print( "need new battery")
             battery_life-= 1
-'''
+# break
 coffe_type = [1,2,3,4,5,6,7,8,9,10]
 people_in_store = 10
-question = input("would u like coffe: ")
 while len(coffe_type) > 0 :
+    question = input("would u like coffe: ")
+
+    if question == "yes" :
+        coffe_type.pop()
+        print("here you are. Have a nice day")
+    elif question == "no":
+        print ("ok have a nice day")
+# break
+floors = [1,2,3]    
+floor_rooms1 = ["q","w","e"]
+floor_rooms2 = ["a","s","d"]
+floor_rooms3 = ["z","x","c"]
+people_in_elevator = 3
+while people_in_elevator > 0 :
+    question = input("what room are you looking for? : ")
     question
-    if input == "yes" :
-        people_in_store -= 1
-        coffe_type.pop
-    if input == "no":
-        people_in_store -=1
-        print (" ok have a nice day")
+    if question in floor_rooms1:
+        print(f"ok heading to floor {floors[0]}")
+        people_in_elevator -=1
+    elif question in floor_rooms2:
+        print(f"ok heading to floor {floors[1]}")
+        people_in_elevator -=1
+    elif question in floor_rooms3:
+        print(f"ok heading to floor {floors[2]}")
+        people_in_elevator -=1
+    else :
+        print("incorrect input")
+
+# break 
+stop_light_on = True 
+red_count = 30
+green_count = 30
+yellow_count = 30
+light_color = [green_count,yellow_count,red_count]
+while stop_light_on == True:
+    if green_count == 30 
+
+    while red_count == 30:
+        red_count -= 1
+        if red_count == 0 :
+'''
